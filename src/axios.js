02f1d136946faaac84a17ac8061e835731ea3a66
Zahidul Islam <suzon.zahid@gmail.com>
2023-09-17 23:35:29 +0600

    add axios

---
+++ src/axios.js
@@ -0,0 +1,12 @@
+// axios.js
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:5000', // Your base URL here
+  timeout: 20000, // Timeout in milliseconds
+  headers: {
+    'Content-Type': 'application/json', // You can add default headers here
+  },
+});
+
+export default axiosInstance;
